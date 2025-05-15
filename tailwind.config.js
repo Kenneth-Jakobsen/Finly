4b413b64f5d100903478b4415d203f08a30eb04c
Ken <jakobsenkenneth@hotmail.no>
2025-05-15 08:31:55 +0200

    x

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}"],
+    content: [
+      "./views/**/*.ejs", // All EJS files in the views folder
+      "./index.js",       // Your main server file
+      "./styles.css",     // Include your CSS file for any custom classes
+      "./views/partials/**/*.ejs", // Include partials folder
+    ],
     theme: {
       extend: {},
     },
     plugins: [require('daisyui')],
-  }
-  
+  };
